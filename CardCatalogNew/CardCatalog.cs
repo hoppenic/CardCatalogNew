3c9b333d059c7b65554279a9ba6272afe351eaab
hoppenic <hoppenic@gmail.com>
2018-06-01 16:25:05 -0500

    update

--- CardCatalogNew/CardCatalog.cs
+++ CardCatalogNew/CardCatalog.cs
@@ -1,90 +1,25 @@
-﻿using System.Linq;
+﻿using System;
 using System.Collections.Generic;
-using CardCatalogNew;
-using System;
+using System.Text;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+using CardCatalogNew;
 
 
 namespace CardCatalogNew
-
 {
     public class CardCatalog
     {
-        public string _filename;
-        private List<Book> books;
+        public string _fileName { get; set; }
+        public List<Book> books { get; set; }
 
+
+        //this is my constructor
         public CardCatalog(string fileName)
-        {
-            _filename = fileName;
-            if (System.IO.File.Exists(fileName))
-            {
-                using (System.IO.FileStream stream = new System.IO.FileStream(fileName, System.IO.FileMode.Open))
-                {
-
-                    System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<Book>));
-                    books = serializer.Deserialize(stream) as List<Book>;
-
-                    //System.Runtime.Serialization.Formatters.Binary.BinaryFormatter binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-                    //books = binaryFormatter.Deserialize(stream) as List<Book>;
-                    stream.Close();
-                }
-            }
-            if (books == null)
-            {
-                books = new List<Book>();
-            }
 
 
 
 
 
-
-        }
-
-
-
-        //put books in an array method
-        private Book[] ListBooks()
-        {
-
-            //can I do a list here?
-            return books.ToArray();
-        }
-
-        //add a book method, taking three arguments
-        public void AddBook(string author, string title, int year)
-        {
-            Book newBook = new Book
-            {
-                Author = author,
-                Title = title,
-                Year = year
-            };
-            books.Add(newBook);
-        }
-
-
-        public void Save()
-        {
-            using (System.IO.FileStream stream = new System.IO.FileStream(this._filename, System.IO.FileMode.OpenOrCreate))
-            {
-
-                //System.Runtime.Serialization.Formatters.Binary.BinaryFormatter binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-                //binaryFormatter.Serialize(stream, books);
-
-                System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<Book>));
-                serializer.Serialize(stream, books);
-
-
-                //have to close file stream
-                stream.Close();
-            }
-
-
-
-
-        }
 
 
 
--- CardCatalogNew/Program.cs
+++ CardCatalogNew/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 using System.Linq;
 using System.Collections.Generic;
-using CardCatalogNew;
+
 using System.IO;
 using System;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -15,59 +15,18 @@ namespace CardCatalogNew
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the name of a file");
-            string filename = Console.ReadLine();
-            CardCatalogNew.CardCatalog cardCatalog = new CardCatalogNew.CardCatalog(filename);
-            Console.Clear();
-            string choice = "";
-            do
-            {
-                //this is my menu
 
-                Console.WriteLine("1.\tList All Books");
-                Console.WriteLine("2.\tAdd A Book");
-                Console.WriteLine("3.\tSave And Exit");
-                choice = Console.ReadLine();
-                Console.Clear();
-                switch (choice)
-                {
-                    case "1":
-                        //for each instance book in my list of books
-                        foreach (Book book in cardCatalog.ListBooks())
-                        {
-                            Console.WriteLine("{0}\t{1}\t{2}", book.Title, book.Author, book.Year);
-                        }
-                        Console.WriteLine("Press enter to continue");
-                        Console.ReadLine();
-                        Console.Clear();
-                        break;
-                    case "2":
-                        Console.WriteLine("Enter a Title");
-                        string title = Console.ReadLine();
-                        Console.WriteLine("Enter an Author");
-                        string author = Console.ReadLine();
-                        int year;
-                        Console.WriteLine("Enter a year");
-                        string yearAsString = Console.ReadLine();
-                        int.TryParse(yearAsString, out year);
-                        cardCatalog.AddBook(author, title, year);
-                        Console.Clear();
-                        break;
-                    case "3":
-                        cardCatalog.Save();
-                        break;
-                    default:
-                        {
-                            Console.WriteLine("Not a Valid Choice, press enter to continue");
-                            Console.ReadLine();
-                            Console.Clear();
-                            break;
-                        }
-                }
+            Console.WriteLine("Enter name of file");
+            string newFile = Console.ReadLine();
 
-            } while (choice != "3");
+
+
+
+
+
+
         }
     }
+    
 
 }
-
