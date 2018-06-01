9d678d07ec01d4233a2aca9f52d4c303b49e96e6
hoppenic <hoppenic@gmail.com>
2018-06-01 13:08:26 -0500

    update

---
+++ CardCatalogNew/CardCatalog.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardCatalogNew
+{
+    public class CardCatalog
+    {
+        private string _filename;
+        private List<Book> books;
+
+        public CardCatalog(string fileName)
+        {
+            _filename = fileName;
+            if (System.IO.File.Exists(fileName))
+            {
+                using (System.IO.FileStream stream = new System.IO.FileStream(fileName, System.IO.FileMode.Open))
+                {
+
+                    System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<Book>));
+                    books = serializer.Deserialize(stream) as List<Book>;
+
+                    //System.Runtime.Serialization.Formatters.Binary.BinaryFormatter binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    //books = binaryFormatter.Deserialize(stream) as List<Book>;
+                    stream.Close();
+                }
+            }
+            if (books == null)
+            {
+                books = new List<Book>();
+            }
+        }
+        public Book[] ListBooks()
+        {
+            return books.ToArray();
+        }
+        public void AddBook(string author, string title, int year)
+        {
+            Book newBook = new Book
+            {
+                Author = author,
+                Title = title,
+                Year = year
+            };
+            books.Add(newBook);
+        }
+        public void Save()
+        {
+            using (System.IO.FileStream stream = new System.IO.FileStream(this._filename, System.IO.FileMode.OpenOrCreate))
+            {
+
+                //System.Runtime.Serialization.Formatters.Binary.BinaryFormatter binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                //binaryFormatter.Serialize(stream, books);
+
+                System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<Book>));
+                serializer.Serialize(stream, books);
+
+                stream.Close();
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
--- CardCatalogNew/Program.cs
+++ CardCatalogNew/Program.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿
+using System.Linq;
+using System.Collections.Generic;
+using CardCatalogNew;
 
-namespace CardCatalogNew
+using System;
+
+public class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Please enter the name of a file");
+        string filename = Console.ReadLine();
+        CardCatalog cardCatalog = new CardCatalog(filename);
+        Console.Clear();
+        string choice = "";
+        do
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("1.\tList All Books");
+            Console.WriteLine("2.\tAdd A Book");
+            Console.WriteLine("3.\tSave And Exit");
+            choice = Console.ReadLine();
+            Console.Clear();
+            switch (choice)
+            {
+                case "1":
+                    foreach (Book book in cardCatalog.ListBooks())
+                    {
+                        Console.WriteLine("{0}\t{1}\t{2}", book.Title, book.Author, book.Year);
+                    }
+                    Console.WriteLine("Press enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    break;
+                case "2":
+                    Console.WriteLine("Enter a Title");
+                    string title = Console.ReadLine();
+                    Console.WriteLine("Enter an Author");
+                    string author = Console.ReadLine();
+                    int year;
+                    Console.WriteLine("Enter a year");
+                    string yearAsString = Console.ReadLine();
+                    int.TryParse(yearAsString, out year);
+                    cardCatalog.AddBook(author, title, year);
+                    Console.Clear();
+                    break;
+                case "3":
+                    cardCatalog.Save();
+                    break;
+                default:
+                    {
+                        Console.WriteLine("Not a Valid Choice, press enter to continue");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    }
+            }
+
+        } while (choice != "3");
     }
+}
+
+
+
+//[Serializable]    //Only needed if I use BinaryFormatter
+public class Book
+{
+    public string Author { get; set; }
+    public string Title { get; set; }
+    public int Year { get; set; }
 }
