e8ae5ee6f8f784963b9f839e12f7556219f82669
hoppenic <hoppenic@gmail.com>
2018-06-02 10:17:40 -0500

    update

--- CardCatalogNew/Program.cs
+++ CardCatalogNew/Program.cs
@@ -5,7 +5,8 @@ using System.IO;
 using System;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
-using CardCatalogNew;
+
+
 
 
 
@@ -19,7 +20,50 @@ namespace CardCatalogNew
 
             Console.WriteLine("Enter your file name");
             string userFile = Console.ReadLine();
+            CardCatalog cardCatalog=new CardCatalog(fileName);
+            Console.Clear();
 
+            string userChoice = "";
+            do
+            {
+                Console.WriteLine("List all Books");
+                Console.WriteLine("Add a book");
+                Console.WriteLine("Save and exit");
+                userChoice = Console.ReadLine();
+                Console.Clear();
+                switch (userChoice)
+                {
+
+                    case "1":
+                        foreach(Book book in cardCatalog.ListOfBooks())
+                        {
+                            Console.WriteLine($"{book.Author}, {book.Title}, {book.Year}");
+                        }
+                        Console.WriteLine("Press enter to continue");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter title");
+                        string title = Console.ReadLine();
+                        Console.WriteLine("Enter author");
+                        string author = Console.ReadLine();
+                        Console.WriteLine("Enter year");
+                        int year = Convert.ToInt32(Console.ReadLine());
+                        cardCatalog.AddBook(title, author, year);
+                        Console.Clear();
+                        break;
+                    case "3":
+                        cardCatalog.Save();
+                        break;
+
+
+
+
+
+                }
+
+            }
 
 
 
@@ -74,16 +118,38 @@ namespace CardCatalogNew
         }
 
 
-        public void AddBook(string bookAuthor,string bookTitle,int bookyear)
+        //method to Add a book
+        public void AddBook(string bookAuthor,string bookTitle,int bookYear)
         {
 
-            a
+            //nerwBook object of Book class
+            Book newBook = new Book
+            {
+                Author = bookAuthor,
+                Title = bookTitle,
+                Year = bookYear
+            };
+
+            books.Add(newBook);
 
         }
 
+        public void Save()
+        {
+
+            using(System.IO.FileStream stream = new System.IO.FileStream(this._fileName, System.IO.FileMode.OpenOrCreate))
+            {
 
 
+                System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<Book>));
+                serializer.Serialize(stream, books);
 
+                stream.Close();
+
+            }
+
+
+        }
 
 
     }
